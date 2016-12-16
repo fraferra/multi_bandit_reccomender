294a5f29e7021ded183e07416d8a56fd16822904
Francesco Ferrari <francescoferrari@Francescos-MacBook-Pro.local>
2016-12-16 16:14:16 +0100

    food loc 2

--- bot.js
+++ bot.js
@@ -9,7 +9,8 @@ const Config = require('./const.js');
 const request = require('request');
 
 
-var getYelpToken = request({
+var getYelpToken = () => {
+  request({
   url: 'https://api.yelp.com/oauth2/token',
   method: 'POST',
   auth: {
@@ -19,11 +20,12 @@ var getYelpToken = request({
   form: {
     'grant_type': 'client_credentials'
   }
-})["access_token"];
-//, function(err, res) {
-  //var json = JSON.parse(res.body);
-  //console.log("Access Token:", json.access_token);
-//});
+}, function(err, res) {
+  var json = JSON.parse(res.body);
+  console.log("Access Token:", json.access_token);
+})
+  return json.access_token;
+};
 
 const getRestaurants = (location, type_food) => {
   const token = getYelpToken();
